935420f1ff4fa7ec619d70c9a8d53dd99467e59c
Kayla <clarkek8@msu.edu>
2014-10-23 17:20:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {  
-  
     $('nav').hover(  
-  
         function() {  
-  
             $(this).css('padding', '5px 15px')  
-         .stop()  
-         .animate({'paddingLeft'    : '25px',  
+            .stop()  
+            .animate({'paddingLeft'    : '25px',  
                      'paddingRight' : '25px',  
                      'backgroundColor':'rgba(0,0,0,0.5)'},  
                      'fast');  
         },  
   
         function() {  
-  
             $(this).css('padding', '5px 25px')  
          .stop()  
          .animate({'paddingLeft'    : '15px',  
