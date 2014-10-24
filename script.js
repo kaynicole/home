932505ae953978c23ae37edc74bb781076de49ea
Kayla <clarkek8@msu.edu>
2014-10-24 15:10:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,4 +24,20 @@ $(document).ready(function() {
     });  
 });  
 
+*/ stuff */
+
+$(document).ready(function() {
+		$('.myMenu > li').bind('mouseover', openSubMenu);
+		$('.myMenu > li').bind('mouseout', closeSubMenu);
+		
+		function openSubMenu() {
+			$(this).find('ul').css('visibility', 'visible');	
+		};
+		
+		function closeSubMenu() {
+			$(this).find('ul').css('visibility', 'hidden');	
+		};
+				   
+	});
+
  
