1240ad39fcee49adf8e074dfa9edc362fb038024
Kayla <clarkek8@msu.edu>
2014-10-24 15:17:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-$(document).ready(function() {  
-    $('nav').hover(  
-        function() {  
-            $(this).css('padding', '5px 15px')  
-            .stop()  
-            .animate({'paddingLeft'    : '25px',  
-                     'paddingRight' : '25px',  
-                     'backgroundColor':'rgba(0,0,0,0.5)'},  
-                     'fast');  
-},  
-  
-        function() {  
-            $(this).css('padding', '5px 25px')  
-            .stop()  
-            .animate({'paddingLeft'    : '15px',  
-                      'paddingRight'      : '15px',  
-                      'backgroundColor' :'rgba(0,0,0,0.2)'},  
-                      'fast');  
-                      
-    }).mousedown(function() {  
-            $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.1)'}, 'fast');  
-      }).mouseup(function() {  
-            $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.5)'}, 'fast');  
-    });  
-});  
-
-
-$(document).ready(function() {
+ $(document).ready(function() {
 		$('.myMenu > li').bind('mouseover', openSubMenu);
 		$('.myMenu > li').bind('mouseout', closeSubMenu);
 		
