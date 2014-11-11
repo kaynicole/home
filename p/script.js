98cbfd40dc1dbc66eac2f54b021ad4c51f5b8b2a
Kayla <clarkek8@msu.edu>
2014-11-11 17:11:29 -0500

    Update script.js

--- p/script.js
+++ p/script.js
@@ -128,3 +128,31 @@ $.fn.sooperfish = function(op) {
 
 };
 
+
+$(document).ready(function() {  
+    $('nav').hover(  
+        function() {  
+            $(this).css('padding', '5px 15px')  
+            .stop()  
+            .animate({'paddingLeft'    : '25px',  
+                     'paddingRight' : '25px',  
+                     'backgroundColor':'rgba(0,0,0,0.5)'},  
+                     'fast');  
+},  
+  
+        function() {  
+            $(this).css('padding', '5px 25px')  
+            .stop()  
+            .animate({'paddingLeft'    : '15px',  
+                      'paddingRight'      : '15px',  
+                      'backgroundColor' :'rgba(0,0,0,0.2)'},  
+                      'fast');  
+                      
+    }).mousedown(function() {  
+            $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.1)'}, 'fast');  
+      }).mouseup(function() {  
+            $(this).stop().animate({'backgroundColor': 'rgba(0,0,0,0.5)'}, 'fast');  
+    });  
+}); 
+
+
